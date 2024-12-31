98d91479c7762bcfacf2b0ac21605de131ed5cf3
astakhov <astakhov@iep.ru>
2024-12-31 17:48:54 +0300

    DZ5 classes

---
+++ README.md
@@ -0,0 +1,3 @@
+# CI classes_and_inheritance
+
+[![Build status](https://ci.appveyor.com/api/projects/status/3n3sryaceyswsego/branch/main?svg=true)](https://ci.appveyor.com/project/pingAST/oop-classes-and-inheritance/branch/main)
---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+// TODO: write your code here
+import Character from './Character';
+import sum from './basic';
+
+console.log('worked');
+
+console.log(sum([1, 2]));
+
+const hero = new Character('Hero', 'Bowman');
+console.log(hero);
